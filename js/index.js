cce3dfef366ba9cc5a16a96fad185bae379421b9
bogrud <bogrud@icloud.com>
2022-05-27 23:52:24 +0300

    function

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 // // Все задания оформить в виде функций:
+
 // // 1 Вывести знак * столько раз сколько задаст пользователь
 
 printStars(6);
@@ -25,23 +26,36 @@ function printNumber (limit1, limit2, num) {
 
 // // 3 Сумма четных элементов массива
 
-let array1 = [2, 13, 206, 17, 10, 23]; // Cумма четных элементов = 218
+let userItem1 = [2, 13, 206, 17, 10, 23]; // Cумма четных элементов = 218
 
-let sum = 0;
-for (i = 0; i < array1.length; i++){
-  if(array1[i]%2 == 0) sum += array1[i];
+task1(userItem1);
+
+function task1(a){
+
+  let sum = 0;
+  for (i = 0; i < a.length; i++){
+    if(a[i]%2 == 0) sum += a[i];
+  }
+  console.log(sum);
+  return sum;
 }
-console.log(sum);
 
 // 4 Обнулите все элементы массива меньше 10
 
-let array2 = [4, 0, -1, -5, 3.14, 0, 0, 100, -4, 2.45, 4, 0, 4];
+let userItem2 = [4, 0, -1, -5, 3.14, 0, 0, 100, -4, 2.45, 4, 0, 4];
 
-for(i = 0; i < array2.length; i++) {
+task2(userItem2);
 
-  if(array2[i] < 10) {
-    array2[i] = 0;
+function task2(a) {
+  for(i = 0; i < a.length; i++) {
+    if(a[i] < 10) {
+      a[i] = 0;
+    }
   }
+  console.log(a);
 }
-console.log(array2);
+
+
+
+
 
